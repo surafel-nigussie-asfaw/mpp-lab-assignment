fe71b3f8a1b712d8fb77c2b6a94c82e386948446
unknown <986878@CS.MUM.EDU>
2018-12-04 16:04:18 -0600

    addBook() Method in admin Controller

--- src/ea/mpp/library/controllers/AdminController.java
+++ src/ea/mpp/library/controllers/AdminController.java
@@ -1,5 +1,33 @@
 package ea.mpp.library.controllers;
 
+import java.util.List;
+
+import ea.mpp.library.entities.*;
+
 public class AdminController {
+
+	public AdminController() {
+
+	}
+
+	public boolean addBook(String title, String ISBN, int maxLeaseDays, List<Author> authors, List<BookCopy> copies) {
+
+		BookInfo bookInfo = new BookInfo(maxLeaseDays, title, ISBN, authors, copies);
+
+//		DAO dataAccess = new DAO();
+//
+//		if (dataAccess.bookExists(bookInfo)) {
+//
+//			return false;
+//		}
+//
+//		else {
+//
+//			return dataAccess.createBook(bookInfo);
+//		}
+
+		
+		return false;
+	}
 
 }
