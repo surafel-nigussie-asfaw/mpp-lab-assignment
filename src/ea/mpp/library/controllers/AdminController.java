b6fc3dd160d1c72f7fd7becd07f2da86d8360bd2
unknown <986878@CS.MUM.EDU>
2018-12-04 16:20:41 -0600

    getBooks() and editBook() in the Admin controller.

--- src/ea/mpp/library/controllers/AdminController.java
+++ src/ea/mpp/library/controllers/AdminController.java
@@ -6,15 +6,18 @@ import ea.mpp.library.entities.*;
 
 public class AdminController {
 
+	/*Data Access Object for Books: To be edited later*/
+//	DAO dataAccess = new DAO();
+	
 	public AdminController() {
 
 	}
 
+	// adding book to the data store	
 	public boolean addBook(String title, String ISBN, int maxLeaseDays, List<Author> authors, List<BookCopy> copies) {
 
 		BookInfo bookInfo = new BookInfo(maxLeaseDays, title, ISBN, authors, copies);
 
-//		DAO dataAccess = new DAO();
 //
 //		if (dataAccess.bookExists(bookInfo)) {
 //
@@ -28,6 +31,34 @@ public class AdminController {
 
 		
 		return false;
+	}
+	
+	
+	//getting all the books from the data store
+	public List<BookInfo> getBooks(){
+		
+//		DAO dataAccess = new DAO();
+		
+//		return dataAccess.getBooks();
+		
+		
+		return null;
+	}
+	
+	
+	//editing an existing book in the store
+	public boolean editBook(BookInfo book) {
+		
+//		if(dataAccess.updateBook(book)) {
+//			
+//			return true;
+//		}
+//		
+//		else 
+//		{
+//			
+//			return false;
+//		}
 	}
 
 }
