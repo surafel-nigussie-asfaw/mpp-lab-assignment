90fdad5429bd8a0a0a22a9f56a1e699a22a50757
Ssekirime Geofrey <ssegeo@gmail.com>
2018-12-04 22:31:26 -0600

    added business logic to create add/edit library member

--- src/ea/mpp/library/controllers/AdminController.java
+++ src/ea/mpp/library/controllers/AdminController.java
@@ -1,5 +1,46 @@
 package ea.mpp.library.controllers;
 
-public class AdminController {
+import java.util.Map;
+import ea.mpp.library.entities.*;
+import ea.mpp.library.data.*;
 
+public class AdminController {
+	private static MemberDAO memberDAO = new MemberDAO();
+	
+	public AdminController() {
+		
+	}
+	
+	/**
+	 * get a list of all library members
+	 * @return
+	 */
+	public Map<Integer, LibraryMember> getLibraryMembers(){
+		return memberDAO.getAll();
+	}
+	
+	/**
+	 * edit library member 
+	 * @param member library member id
+	 * @return LibraryMember edited copy of the library member details
+	 */
+	public LibraryMember editLibraryMember(LibraryMember member) {
+		memberDAO.update(member.getLibraryMemberId(), member);
+		
+		return member;
+	}
+	
+	/**
+	 * add new library members
+	 * @param member a fresh copy of the library member
+	 * @return
+	 */
+	public LibraryMember addLibraryMember(Person personDetails) {
+		Integer libraryMemberId = memberDAO.generateUniqueId();
+		LibraryMember member = new LibraryMember(libraryMemberId, personDetails);
+		
+		memberDAO.add(libraryMemberId, member);
+		
+		return member;
+	}
 }
--- src/ea/mpp/library/data/MemberDAO.java
+++ src/ea/mpp/library/data/MemberDAO.java
@@ -2,11 +2,12 @@ package ea.mpp.library.data;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 import ea.mpp.library.entities.LibraryMember;
 
 public class MemberDAO {
-	private Map<Integer, LibraryMember> libraryMemberDataMap = new HashMap<Integer, LibraryMember>();
+	private static Map<Integer, LibraryMember> libraryMemberDataMap = new HashMap<Integer, LibraryMember>();
 	
 	public LibraryMember add(Integer key, LibraryMember value) {
 		return libraryMemberDataMap.put(key, value);
@@ -19,8 +20,27 @@ public class MemberDAO {
 	public LibraryMember get(Integer key) {
 		return libraryMemberDataMap.get(key);
 	}
+	
+	public Map<Integer, LibraryMember> getAll() {
+		return libraryMemberDataMap;
+	}
 
 	public LibraryMember delete(Integer key) {
 		return libraryMemberDataMap.remove(key);
+	}
+	
+	/**
+	 * generate unique random IDs for use as library member IDs
+	 * @return
+	 */
+	public static Integer generateUniqueId() {
+		Random rand = new Random();
+		Integer id = (Integer)rand.nextInt(20000);
+
+		while (libraryMemberDataMap.containsKey(id)) {
+			id = (Integer)rand.nextInt(20000);
+		};
+
+		return id;
 	}
 }
