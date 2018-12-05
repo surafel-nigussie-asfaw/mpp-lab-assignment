dc6219a2ceeee6641ecaf4f43f5eeb1e924023ac
stevenkatabalwa <Skatabalwa@mum.edu>
2018-12-05 11:48:30 -0600

    Addded code for BookUI and implementation in the AdminController

--- src/ea/mpp/library/data/BookInfoDAO.java
+++ src/ea/mpp/library/data/BookInfoDAO.java
@@ -8,12 +8,12 @@ import ea.mpp.library.entities.BookInfo;
 public class BookInfoDAO {
 	private Map<String, BookInfo> bookInfoDataMap = new HashMap<String, BookInfo>();
 	
-	public BookInfo add(String title, BookInfo value) {
-		return bookInfoDataMap.put(title, value);
+	public BookInfo add(BookInfo value) {
+		return bookInfoDataMap.put(value.getTitle(), value);
 	}
 
-	public BookInfo update(String title, BookInfo value) {
-		return bookInfoDataMap.put(title, value);
+	public BookInfo update(BookInfo value) {
+		return bookInfoDataMap.put(value.getTitle(), value);
 	}
 
 	public BookInfo get(String title) {
